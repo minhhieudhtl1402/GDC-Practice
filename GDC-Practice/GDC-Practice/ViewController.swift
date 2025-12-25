f8cd078f93042ebbb3982a080b80fecf13d82a7e
vmhieu <minghieuu@gmail.com>
2025-12-25 11:56:21 +0700

    Cancel task

--- GDC-Practice/GDC-Practice/ViewController.swift
+++ GDC-Practice/GDC-Practice/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+/// Xem xét vì hoạt động chưa đúng
+enum TaskStatus: String {
+    case notRunning
+    case running
+    case cancelled
+    case completed
+}
 
 class ViewController: UIViewController {
-
+    
+    var taskStatus: TaskStatus = .notRunning {
+        didSet {
+            DispatchQueue.main.async {
+                self.taskStatusLabel.text = self.taskStatus.rawValue
+            }
+        }
+    }
+    @IBOutlet weak var taskStatusLabel: UILabel!
+    var work: DispatchWorkItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.sync {
-            print("Cause deadlock or not ?")
+        taskStatus = .notRunning
+    }
+    
+    @IBAction func stopTask(_ sender: Any) {
+        work?.cancel()
+        taskStatus = .cancelled
+    }
+    
+    @IBAction func startTask(_ sender: Any) {
+        work = DispatchWorkItem {
+            self.taskStatus = .running
+            Thread.sleep(forTimeInterval: 10)
+            self.taskStatus = .completed
         }
+        guard let work else { return }
+        DispatchQueue.global().async(execute: work)
     }
 }
 
