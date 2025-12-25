8ab1da5a329b113a008acf26a33d4d207f44aba8
vmhieu <minghieuu@gmail.com>
2025-12-25 11:22:42 +0700

    Race condition

--- GDC-Practice/GDC-Practice/ViewController.swift
+++ GDC-Practice/GDC-Practice/ViewController.swift
@@ -8,11 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var sharedValue: Int = 0
+    
+    private var number: Int = 0
+    private var strings: [String: Int] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.sync {
-            print("Cause deadlock or not ?")
+        raceConditionV2()
+    }
+    
+    func raceCondition() {
+        DispatchQueue.global().async {
+            let group = DispatchGroup()
+            
+            let someQueue = DispatchQueue(label: "someQueue", attributes: .concurrent)
+            group.enter()
+            someQueue.async {
+                for _ in 0...10 {
+                    self.sharedValue += 1
+                    print("❤️ shareValue \(self.sharedValue)")
+                }
+                group.leave()
+            }
+            
+            let anotherQueue = DispatchQueue(label: "anotherQueue", attributes: .concurrent)
+            group.enter()
+            anotherQueue.async {
+                for _ in 0...10 {
+                    self.sharedValue += 1
+                    print("🥥 shareValue \(self.sharedValue)")
+                }
+                group.leave()
+            }
+            group.wait()
+            print("Shared after alter \(self.sharedValue)")
+        }
+    }
+    
+    func raceConditionV2() {
+        let concurrentQueue = DispatchQueue(label: "", attributes: .concurrent)
+        // Thread 1
+        concurrentQueue.async {
+            for _ in 0...10 {
+                self.number += 1
+                self.strings["🔴 \(self.number)"] = self.number
+                print("🔴: \(self.number)")
+            }
+        }
+        
+        // Thread 2
+        concurrentQueue.async {
+            for _ in 0...10 {
+                self.number += 1
+                self.strings["🔵 \(self.number)"] = self.number
+                print("🔵: \(self.number)")
+            }
+        }
+        
+        // show result after 3.0 secs
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            print("Number = \(self.number)")
+            for item in self.strings {
+                print(item)
+            }
         }
     }
 }
